7c3387394bb4fb8a9a853ed0100cb3e93ead6804
qmy <275394982@qq.com>
2018-11-19 18:16:56 +0800

    test

--- 反向运算符重载/反向算术运算符重载.py
+++ 反向运算符重载/反向算术运算符重载.py
@@ -11,8 +11,17 @@ class Mylist:
     def __rmul__(self, other):
         return self.l*other
 
+    def __add__(self, other):
+        return self.l+other
+
+    def __radd__(self, other):
+        return other+self.l
+
 
 l=Mylist([1,2,3])
 print(l)
 print(l*2)
 print(3*l)
+
+print(l+[4,5,6])
+print([4,5,6]+l)
